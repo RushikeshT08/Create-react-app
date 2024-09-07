8d9dbca4925dc456232e4273865b98e95c0c2d90
Rushikesh <rushikeshthange3884@gmail.com>
2024-09-07 15:59:35 +0530

    Updates

---
+++ precache-manifest.488606d33c185417986283a32e5d0160.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d577ad44521dad071a18d386ef464cd",
+    "url": "/nasa-apod-gallery-app/index.html"
+  },
+  {
+    "revision": "c318f97028475e34d8e2",
+    "url": "/nasa-apod-gallery-app/static/css/main.9a1da716.chunk.css"
+  },
+  {
+    "revision": "47d7f9ebc7e2e14a8655",
+    "url": "/nasa-apod-gallery-app/static/js/2.3ea9490e.chunk.js"
+  },
+  {
+    "revision": "c318f97028475e34d8e2",
+    "url": "/nasa-apod-gallery-app/static/js/main.e459f4b8.chunk.js"
+  },
+  {
+    "revision": "020212fc45a9a3061a6d",
+    "url": "/nasa-apod-gallery-app/static/js/runtime~main.48c28c96.js"
+  },
+  {
+    "revision": "1fe66bf6858793bd00e8ca5190663478",
+    "url": "/nasa-apod-gallery-app/static/media/darkrift-space.1fe66bf6.jpg"
+  }
+]);
